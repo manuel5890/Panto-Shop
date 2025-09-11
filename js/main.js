1020ba9725d0993fa6b026cd73fac981e71d140e
Manuel Calderon <manuelcalderon5890@gmail.com>
2025-09-11 16:04:19 -0500

    realice la section de testimonials o reviews

--- js/main.js
+++ js/main.js
@@ -37,4 +37,42 @@ $(document).ready(function(){
         // instead of a settings object
     ]
     });
+
+    $('.slider-review').slick({
+    dots: false,
+    infinite: false,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+        }
+        },
+        {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+    });
   });
