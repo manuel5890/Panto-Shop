0446469bcd49378948e6093b7a6eb51be21c7f16
Manuel Calderon <manuelcalderon5890@gmail.com>
2025-09-11 17:45:44 -0500

    finalice el footer e inicie con el responsive

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
         {
         breakpoint: 1024,
         settings: {
-            slidesToShow: 3,
+            slidesToShow: 2,
             slidesToScroll: 1,
             infinite: true,
             dots: false
@@ -21,7 +21,7 @@ $(document).ready(function(){
         {
         breakpoint: 600,
         settings: {
-            slidesToShow: 2,
+            slidesToShow: 1,
             slidesToScroll: 1
         }
         },
